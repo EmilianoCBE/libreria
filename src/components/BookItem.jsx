d1a0b0a288258b983b3a0f5a5ec7cce80e30fffc
EmilianoCBE <e.carrizales1305@gmail.com>
2024-07-15 20:15:04 -0600

    Lecture list count and books available fixed

--- src/components/BookItem.jsx
+++ src/components/BookItem.jsx
@@ -1,24 +1,39 @@
-import { useContext } from 'react'
-import './BookItem.css'
-import { LectureList } from '../App'
+import { useContext } from 'react';
+import './BookItem.css';
+import { LectureList } from '../App';
 
 export const BookItem = (book) => {
-  const {lectureList, setLectureList, setList, setListCount} = useContext(LectureList)
-  const bookExistsInLectureList = lectureList.find((item) => item.title === book.book.title)
+  const {
+    lectureList,
+    setLectureList,
+    setList,
+    setListCount,
+    booksCount,
+    setBooksCount,
+  } = useContext(LectureList);
+
+  const bookExistsInLectureList = lectureList.find((item) => item.title === book.book.title);
 
   const addLectureList = () => {
-    if (bookExistsInLectureList) return
-    setLectureList([...lectureList, book.book])
-    setList(true)
-    setListCount(lectureList.length + 1)
-  }
+    if (bookExistsInLectureList) {
+      setLectureList(lectureList.filter((item) => item.title !== book.book.title));
+      setBooksCount(booksCount + 1);
+      setListCount(lectureList.length - 1);
+    } else {
+      setLectureList([...lectureList, book.book]);
+      setBooksCount(booksCount - 1);
+      setListCount(lectureList.length + 1);
+    }
+    setList(true);
+  };
 
   return (
-    <div className={
-      bookExistsInLectureList ? "book-item selected" : "book-item"
-    } onClick={addLectureList}>
+    <div
+      className={bookExistsInLectureList ? 'book-item selected' : 'book-item'}
+      onClick={addLectureList}
+    >
       <h3>{book.book.title}</h3>
-      <img src={book.book.cover} alt={book.book.title} width={200}/>
+      <img src={book.book.cover} alt={book.book.title} width={200} />
     </div>
-  )
-}
+  );
+};
--- src/components/BooksGrid.jsx
+++ src/components/BooksGrid.jsx
@@ -1,36 +1,37 @@
-import { useContext, useEffect, useState } from 'react'
+import { useContext, useEffect, useState } from 'react';
 import { Row, Col } from "antd";
 import { BookItem } from './BookItem';
 import { LectureList } from '../App';
 
 export const BooksGrid = () => {
-  const [books, setBooks] = useState([])
-  const [filter, setFilter] = useState('all')
-  const {booksCount, setBooksCount} = useContext(LectureList)
+  const [books, setBooks] = useState([]);
+  const [filter, setFilter] = useState('all');
+  const { booksCount, setBooksCount, lectureList } = useContext(LectureList);
 
-    useEffect(() => {
-      fetch('/src/data/books.json')
-        .then((response) => response.json())
-        .then((data) => {
-          setBooksCount(data.library.length)
-          if (filter === 'all') {
-            return setBooks(data.library)
-          } else {
-            const filteredBooks = data.library.filter((book) => book.book.genre === filter)
-            setBooks(filteredBooks)
-            setBooksCount(filteredBooks.length)
-          }
-        })
+  useEffect(() => {
+    fetch('/src/data/books.json')
+      .then((response) => response.json())
+      .then((data) => {
+        let filteredBooks = data.library;
 
-    }, [filter, setBooksCount])
+        if (filter !== 'all') {
+          filteredBooks = filteredBooks.filter((book) => book.book.genre === filter);
+        }
 
-    const setFilterBooks = (filter) => {
-      setFilter(filter)
-    }
+        const availableBooksCount = filteredBooks.length - lectureList.filter(item => filteredBooks.some(book => book.book.title === item.title)).length;
+
+        setBooks(filteredBooks);
+        setBooksCount(availableBooksCount);
+      });
+  }, [filter, lectureList, setBooksCount]);
+
+  const setFilterBooks = (filter) => {
+    setFilter(filter);
+  };
 
   return (
     <>
-      <h3>Books Avaiable: {booksCount}</h3>
+      <h3>Books Available: {booksCount}</h3>
       <select
         value={filter}
         onChange={(e) => setFilterBooks(e.target.value)}
@@ -42,24 +43,20 @@ export const BooksGrid = () => {
         <option value="Terror">Terror</option>
       </select>
       <Row gutter={{ xs: [8, 8], sm: [16, 16], md: [24, 24], lg: [24, 24] }}>
-        {
-          books.map((book, index) => (
-            
-            <Col
-              xs={12}
-              sm={12}
-              md={6}
-              lg={{
-                flex: "25%",
-              }}
-              key={index}
-            >
-              
-              <BookItem {...book}/>
-            </Col>
-          ))
-        }
+        {books.map((book, index) => (
+          <Col
+            xs={12}
+            sm={12}
+            md={6}
+            lg={{
+              flex: "25%",
+            }}
+            key={index}
+          >
+            <BookItem {...book} />
+          </Col>
+        ))}
       </Row>
     </>
-  )
-}
+  );
+};
